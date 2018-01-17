4532ac9832565388044a391c3ddcbdd86d838f2a
barolina <kittenlora@yandex.ru>
2018-01-17 21:08:08 +0300

    wsgi

--- config/wsgi.py
+++ config/wsgi.py
@@ -43,21 +43,5 @@ application = get_wsgi_application()
 # application = HelloWorldApplication(application)
 
 
-""
-WSGI config for nngconsulting project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nngconsulting.settings")
-
-application = get_wsgi_application()
 
 
